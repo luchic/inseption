b7e95413d0a70cdce7407c1a0b6aba8f2c92dded
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-03 11:56:33 +0100

    mariadb fix setup files

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
-mysqld --skip-networking --socket=/var/run/mysqld/mysqld.sock &
+
+if [[ -z "$DB_PASSWORD_FILE" ]]; then
+	echo "no secret: DB_PASSWORD_FILE"
+	exit 1
+else
+	DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
+fi
+
+if [[ -z "$DB_ADMIN_PASSWORD_FILE" ]]; then
+	echo "no secret: DB_ADMIN_PASSWORD_FILE"
+	exit 1
+else
+	DB_ADMIN_PASSWORD=$(cat "$DB_ADMIN_PASSWORD_FILE")
+fi
+
+if [[ -z "$DB_ADMIN_USER_FILE" ]]; then
+	echo "no secret: DB_ADMIN_USER_FILE"
+	exit 1
+else
+	DB_ADMIN_USER=$(cat "$DB_ADMIN_USER_FILE")
+fi
+
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+
+mysqld --user=root &
 
 until mysqladmin ping --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 1
 done
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+echo "Try to Initializing database..."
 
-    echo "Initializing database..."
-    mysql -u root < /conf/init.sql
-fi
+    mysql -u root <<EOF
+create database if not exists ${DB_NAME};
 
+create user if not exists '${DB_USER}'@'%' identified by '${DB_PASSWORD}';
+grant all privileges on ${DB_NAME}.* to '${DB_USER}'@'%';
+
+create user if not exists '${DB_ADMIN_USER}'@'%' identified by '${DB_ADMIN_PASSWORD}';
+grant all privileges on *.* to '${DB_ADMIN_USER}'@'%' with grant option;
+
+EOF
+
+echo "Is finished..."
 mysqladmin -u root shutdown
 
 exec "$@"
+
+
+# create database if not exists ${DB_NAME};
+
+# create user if not exists ${DB_USER}@'%' identified by '${DB_PASSWORD}';
+# grant all privileges on '${DB_NAME}'.* to '${DB_USER}'@'%';
+
+# create user if not exists ${DB_ADMIN_USER}@'%' identified by '${DB_ADMIN_PASSWORD}';
+# grant all privileges on *.* to '${DB_ADMIN_USER}'@'%' with grant option;
+
