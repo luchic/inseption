f3a878476ddecb241c4cdc4a090bf2f6d5832736
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-03 13:15:34 +0100

    Update setup script for wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1;" > /dev/null 2>&1
+# mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1;"
+
+if [[ -z "$DB_PASSWORD_FILE" ]]; then
+	echo "no secret: DB_PASSWORD_FILE"
+	exit 1
+else
+	DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
+fi
+
+until mysql -h"$DB_HOST" -P 3306 -u"$DB_USER" -p"$DB_PASSWORD" -D"$DB_NAME" -e "SELECT 1;" > /var/null 2>&1
 do
     echo "Waiting for MariaDB..."
     sleep 2
@@ -8,7 +17,11 @@ done
 
 cd /var/www/html/wordpress
 
+echo "Try to Setup wp-config..."
+
 if [ ! -f wp-config.php ]; then
+	echo "Setuping wp-config..."
+
     cp wp-config-sample.php wp-config.php
 
     sed -i "s/database_name_here/$DB_NAME/" wp-config.php
@@ -16,5 +29,7 @@ if [ ! -f wp-config.php ]; then
     sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
     sed -i "s/localhost/$DB_HOST/" wp-config.php
 fi
+
+echo "Finished to setup wp-config..."
 
 exec "$@"
