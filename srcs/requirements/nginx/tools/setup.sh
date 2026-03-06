7b45b93e63f22fb02de751c0c55a20780680c12a
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-06 13:08:43 +0100

    Ngnix: update setup.sh file: add load certificates

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-echo "Starting ngninix..."
+export SSL_FOLDER="/etc/nginx/ssl" 
+
+echo "Try to setup certificates ..."
+
+
+if [ ! -d "$SSL_FOLDER" ]; then
+  echo "Creating ssl folder ${SSL_FOLDER}"
+  mkdir -p "$SSL_FOLDER"
+fi
+
+if [[ -z "$NGNIX_CRT_FILE" ]]; then
+	echo "Error: no certificate for ngnix"
+	exit 1
+else
+	cp /run/secrets/ngnix_crt "${SSL_FOLDER}/nginx.crt"
+fi
+
+if [[ -z "$NGNIX_KEY_FILE" ]]; then
+	echo "Error: no key for certificate"
+	exit 1
+else
+	cp /run/secrets/ngnix_key "${SSL_FOLDER}/nginx.key"
+fi
+
+# ssl_certificate      /etc/nginx/ssl/nginx.crt;
+# ssl_certificate_key  /etc/nginx/ssl/nginx.key;
+
+# NGNIX_CRT_FILE=/run/secrets/ngnix_crt
+# NGNIX_KEY_FILE=/run/secrets/ngnix_key
+
+echo "Starting ngninix ..."
 
 exec "$@"
